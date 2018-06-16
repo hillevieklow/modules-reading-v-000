b19efc19af53239ed70a6482384c0e09d0b974ec
Hillevi Eklöw <hillevieklow@gmail.com>
2018-06-16 17:47:09 +0000

    automatically backed up by learn

--- lib/kid.rb
+++ lib/kid.rb
@@ -2,6 +2,8 @@ require_relative './fancy_dance.rb'
 class Kid
   include Dance
   extend MetaDancing
+  extend FancyDance::ClassMethods
+  include FancyDance::InstanceMethods
 
   attr_accessor :name
 
