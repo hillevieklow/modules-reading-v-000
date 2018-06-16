fc9f38f08c925eb906becaae9f77ed5720081307
Hillevi Eklöw <hillevieklow@gmail.com>
2018-06-16 17:49:02 +0000

    automatically backed up by learn

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -1,7 +1,7 @@
 require_relative './fancy_dance.rb'
 
 class Dancer
-  include Dance
+  include Dancer
   extend MetaDancing
   extend FancyDance::ClassMethods
   include FancyDance::InstanceMethods
